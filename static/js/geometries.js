59c9c0aa75c7881ec5cde4b4675c4c18eb03a6a9
piderking <piderking8@gmail.com>
2024-05-21 06:39:37 -0700

    Added Untested support for 3D text elements

--- static/js/geometries.js
+++ static/js/geometries.js
@@ -1,4 +1,6 @@
 import * as THREE from "three";
+import { TextGeometry } from './util/TextGeometrey.js'
+import { FontLoader } from "./util/FontLoader.js"
 /**
  * Represents a 3D Box.
  * @constructor
@@ -6,6 +8,36 @@ import * as THREE from "three";
  * @param {object} scale - x, y, z of the scale, default to 1
  * @param {object} position - x, y, z of the box, default to 
  */
+
+
+const loader = new FontLoader();
+
+const loadFont = (url) => {
+    let lFont;
+    const font = loader.load(
+        // resource URL
+        url,
+    
+        // onLoad callback
+        function ( font ) {
+            // do something with the font
+            console.log( font );
+            lFont = font
+        },
+    
+        // onProgress callback
+        function ( xhr ) {
+            console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
+        },
+    
+        // onError callback
+        function ( err ) {
+            console.log( 'An error happened' );
+        }
+    );
+    console.log(lFont)
+    return lFont
+}
 export class Box{
     constructor(scene, uuid, scale, position, color){
         this.scene = scene;
@@ -24,6 +56,87 @@ export class Box{
         // Set ID
         this.uuid = uuid
         //this._mesh.uuid = uuid;
+
+        // Add to Scene
+        this.scene.add(this._mesh)
+
+        console.log(`Box Created of UUID: ${this.uuid}`)
+
+    }
+    object(){
+        return this._mesh
+    }
+    move(x, y, z){
+        this._mesh?.position.set(x, y, z);
+    }
+    
+    remove(){
+        this._geo.dispose();
+        this._mat.dispose();
+        this.scene.remove( this._mesh );
+        console.log(`Box Removed of UUID: ${this.uuid}`)
+    }
+}
+export class Text{
+    constructor(scene, uuid, text, options){
+        // Assign Parameters
+        this.scene = scene;
+        this.text = text;
+        this.uuid = uuid
+
+        // Asign Options
+        this.scale = options.scale
+        this.position = options.position
+        this.color = options.color
+        this._url = options.url
+
+        // Finish with Options
+        this.options = options
+
+        // Auto Default if opp is Null
+        this.scale ??= { x:1, y:1, z:1}
+        this.position ??= { x:1, y:1, z:1}
+        this.color ??= `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`
+        this._url ??= "fonts/lmk.json"
+        this.font = loadFont(
+            this._url
+        )
+        // Text Options
+        this.options.texto ??= {
+            size: 80,
+            depth: 5,
+            curveSegments: 12,
+            bevelEnabled: true,
+            bevelThickness: 10,
+            bevelSize: 8,
+            bevelOffset: 0,
+            bevelSegments: 5
+        }
+        
+        // Geometric things
+        this._geo = new TextGeometry( this.text, {
+            font: this.font,
+            size: this.options.texto.size,
+            depth: this.options.texto.depth,
+            curveSegments: this.options.texto.curveSegments,
+            bevelEnabled: this.options.texto.bevelEnabled,
+            bevelThickness: this.options.texto.bevelThickness,
+            bevelSize: this.options.texto.bevelSize,
+            bevelOffset: this.options.texto.bevelOffset,
+            bevelSegments: this.options.text.bevelSegments
+        } );
+        this._mat = new THREE.MeshBasicMaterial({
+            color: new THREE.Color(
+                this.color
+            )
+        });
+        this._mesh = new THREE.Mesh(this._geo, this._mat);
+        
+        // Position Geometry
+        
+        this._mesh.position.set(this.position.x, this.position.y, this.position.z);
+        
+
 
         // Add to Scene
         this.scene.add(this._mesh)
